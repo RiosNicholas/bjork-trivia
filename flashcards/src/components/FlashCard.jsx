5338d0b2e563b05fa1c869a1d96bda30726faf50
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-24 19:25:06 -0400

    Center flashcard container

--- flashcards/src/components/FlashCard.jsx
+++ flashcards/src/components/FlashCard.jsx
@@ -9,8 +9,8 @@ const FlashCard = () => {
         },
     ];
     return (
-        <div className="flex flex-col justify-center hover:cursor-pointer m-36">
-            <div className="flex flex-col justify-center content-center align-middle p-10 w-1/3 h-40 bg-orange-200 rounded-lg shadow-lg text-center">
+        <div className="flex flex-col justify-center items-center h-full m-36">
+            <div className="flex flex-col justify-center content-center align-middle p-10 w-1/3 h-48 bg-orange-200 rounded-lg shadow-lg text-center">
                 <h2 className="font-bold uppercase text-xl m-1">Question goes here.</h2>
                 <p className="font-normal text-lg capitalize m-1">Answer goes here</p>
             </div>
@@ -19,4 +19,4 @@ const FlashCard = () => {
     );
 };
 
-export default FlashCard
+export default FlashCard;
