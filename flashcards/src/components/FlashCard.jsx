0529feddb6591d517fb719462a8fad77dca3b70a
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-24 16:48:19 -0400

    Install Tailwind CSS

---
+++ flashcards/src/components/FlashCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FlashCard = () => {
+    return (
+        <div>
+            <h2>
+                Question goes here.
+            </h2>
+            <p>
+                Answer goes here?.
+            </p>
+        </div>
+    );
+};
+
+export default FlashCard
