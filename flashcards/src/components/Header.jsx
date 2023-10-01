6c3ad5293408463554e96547f94a4592bd938451
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-01 18:55:49 -0400

    Make header more responsive

--- flashcards/src/components/Header.jsx
+++ flashcards/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <div className="flex flex-col items-center justify-center bg-purple-300">
-            <img className="w-1/4 p-8" src={BjorkLogo} alt="Bjork Logo" />
+            <img className="w-1/4 md:p-8 p-4" src={BjorkLogo} alt="Bjork Logo" />
         </div>
     );
 };
