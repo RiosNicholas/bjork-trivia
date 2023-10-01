375392279944e2c976fd12bd4c85c5ae3af94f2a
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-01 18:46:09 -0400

    Rename file

---
+++ flashcards/src/components/FlashCardButtons.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import questionBank from '../assets/questionBank';
+import ArrowButtons from "./ArrowButtons";
+
+const FlashCardButtons = ({ onRandomize, currentQuestion, setCurrentQuestion, handleLeftArrowClick, handleRightArrowClick }) => {
+    const randomizeQuestion = () => {
+        onRandomize();
+    };    
+
+    return (
+        <div className="flex items-center justify-center h-full">
+            <ArrowButtons 
+                onLeftArrowClick={handleLeftArrowClick}
+                onRightArrowClick={handleRightArrowClick} 
+                currentQuestion={currentQuestion}
+                setCurrentQuestion={setCurrentQuestion} // Pass setCurrentQuestion
+            />
+            <div className="flex flex-col w-40 justify-center m-2" >
+                <button onClick={randomizeQuestion} className="bg-blue-600 text-white rounded-lg shadow-lg w-40 m-2" id='randomize-button'>Randomize</button>
+                <p className="text-white italic text-sm text-center font-medium">Number of Cards: {questionBank.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default FlashCardButtons;
