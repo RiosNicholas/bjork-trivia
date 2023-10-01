87bc73569126de481b5221b5da05166da958a836
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-01 18:46:22 -0400

    Update import after file rename

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import FlashCard from './components/FlashCard.jsx'
-import FlashCardButton from './components/FlashCardButton'
+import FlashCardButton from './components/FlashCardButtons'
 import FlashCardContainer from './components/FlashCardContainer'
 
 function App() {
