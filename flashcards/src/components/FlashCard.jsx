347c29a62f401dfa63a4f8ced25f101822275910
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-01 17:09:45 -0400

    Make flashcard width dynamic and responsive

--- flashcards/src/components/FlashCard.jsx
+++ flashcards/src/components/FlashCard.jsx
@@ -1,74 +1,17 @@
 import React, { useState } from "react";
-import FlashCardButton from "./FlashCardButton";
+import questionBank from "../assets/questionBank";
 
-const FlashCard = () => {
-    // TODO: Move this out of the component and into a separate file
-    const questionBank = [
-        {
-            question: "What is Björk's full name?",
-            answer: "Björk Guðmundsdóttir"
-        },
-        {
-            question: "In which year was Björk born?",
-            answer: "1965"
-        },
-        {
-            question: "Which Icelandic band was Björk a part of before her solo career?",
-            answer: "The Sugarcubes"
-        },
-        {
-            question: "What is the title of Björk's debut solo album released in 1993?",
-            answer: "Debut"
-        },
-        {
-            question: "Björk won an Academy Award for Best Original Song for which film's soundtrack?",
-            answer: "Dancer in the Dark"
-        },
-        {
-            question: "In which music video did Björk famously wear a swan dress?",
-            answer: "It's Oh So Quiet"
-        },
-        {
-            question: "Which critically acclaimed album by Björk was released in 1997 and heavily features electronic music?",
-            answer: "Homogenic"
-        }
-    ];
-    
-    /* DEFINING REACT HOOKS */ 
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [showAnswer, setShowAnswer] = useState(false);
-
-    const revealAnswer = () => {
-        setShowAnswer(!showAnswer);
-    };
-
-    /* CALLBACK FUNCTION TO RANDOMIZE THE QUESTIOn DISPLAYED */
-    const randomizeQuestion = () => {
-        const randomIndex = Math.floor(Math.random() * questionBank.length);
-        
-        // Updating the state 
-        setCurrentQuestion(randomIndex);
-        
-        // Hide the answer when changing the question
-        setShowAnswer(false); 
-    };
-
+const FlashCard = ({ currentQuestion, showAnswer, revealAnswer, questionBank }) => {
     return (
-        <div className="flex flex-col justify-center items-center h-full m-36" id="game-container">
-            <div onClick={revealAnswer} className="flex flex-col justify-center content-center align-middle p-10 w-1/3 h-48 bg-orange-200 rounded-lg shadow-lg text-center hover:cursor-pointer">
-                <h2 className="font-bold uppercase text-xl m-1">
-                    {questionBank[currentQuestion].question}
-                </h2>
-                {showAnswer && (
-                    <p className="font-normal text-lg capitalize m-1">
-                        {questionBank[currentQuestion].answer}
-                    </p>
-                )}
-            </div>
-            <div className="flex flex-col">
-                <FlashCardButton onRandomize={randomizeQuestion} />
-                  <p className="text-white italic text-sm text-center font-medium">Number of Cards: {questionBank.length}</p>
-            </div>
+        <div onClick={revealAnswer} className="flex flex-col justify-center content-center align-middle p-10 w-full max-w-lg h-56 bg-orange-200 rounded-lg shadow-lg text-center hover:cursor-pointer">
+            <h2 className="font-bold uppercase text-xl m-1">
+                {questionBank[currentQuestion].question}
+            </h2>
+            {showAnswer && (
+                <p className="font-normal text-lg capitalize m-1">
+                    {questionBank[currentQuestion].answer}
+                </p>
+            )}
         </div>
     );
 };
