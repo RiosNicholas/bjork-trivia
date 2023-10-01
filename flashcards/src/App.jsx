588e4749247826ae50a1a508b721e6602d8012d4
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-01 16:51:48 -0400

    Move button to app.jsx

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import FlashCard from './components/FlashCard.jsx'
+import FlashCardButton from './components/FlashCardButton'
 
 function App() {
-  
   return (
     <>
       <Header />
       <FlashCard /> 
+      <div className="flex flex-col">
+        {/* <FlashCardButton onRandomize={randomizeQuestion} /> */}
+        {/* <p className="text-white italic text-sm text-center font-medium">Number of Cards: {questionBank.length}</p> */}
+        <FlashCardButton />
+      </div>
     </>
   );
 };
